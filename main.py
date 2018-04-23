23403efed37b4080b430e30a27f1a400cd29f7f7
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-04-23 10:36:00 -0700

    Improved readability. Added human readable time.

--- main.py
+++ main.py
@@ -5,6 +5,71 @@ import time
 import sys
 import secrets
 
+###############################################################################
+
+#May get an error from this...
+def print_exception():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
+
+###############################################################################
+
+def is_an_image(url):
+	if url[-3:] in ['jpg', 'png'] or url[-4:] in ['jpeg']:
+		return True
+	return False
+
+###############################################################################
+
+def human_time(time):
+	hours = int(time)
+	minutes = (time*60) % 60
+	seconds = (time*3600) % 60
+	return "%d hr %02d mins %02d sec" % (hours, minutes, seconds)
+
+def get_avg_created_to_front():
+	cursor.execute('select AVG(discovered - created_utc)/60/60.0 from posts;')
+	return cursor.fetchone()[0]
+
+
+def get_avg_front_life():
+	cursor.execute('select AVG(lastseen - discovered)/60/60.0 from posts;')
+	return cursor.fetchone()[0]
+
+###############################################################################
+
+def is_id_in_db(sub):
+	cursor.execute('SELECT EXISTS(SELECT 1 FROM posts WHERE id=?)', (sub.id,))
+	return bool(cursor.fetchone()[0])
+	
+def add_sub_to_db(sub):
+	cursor.execute(
+		'INSERT INTO posts (id, title, url, subreddit, subreddit_id, created_utc, discovered) VALUES (?, ?, ?, ?, ?, ?, ?)',
+		(
+			sub.id,
+			sub.title,
+			sub.url,
+		   	str(sub.subreddit),
+		    sub.subreddit_id,
+		    int(sub.created_utc),
+			int(time.time())
+		)
+	)
+	conn.commit()
+
+def update_lastseen(sub):
+	cursor.execute(
+		'UPDATE posts SET lastseen=? WHERE id=?',
+		(int(time.time()), sub.id)
+	)
+	conn.commit()
+
+###############################################################################
+
+
 #db will be creating if it does not exist
 conn = sqlite3.connect("memer.db", timeout=11)
 cursor = conn.cursor()
@@ -33,57 +98,6 @@ if cursor.fetchone()[0] == 0:
 
 	cursor.execute(create_posts_table)
 
-#May get an error from this...
-def print_exception():
-    exc_type, exc_obj, tb = sys.exc_info()
-    f = tb.tb_frame
-    lineno = tb.tb_lineno
-    filename = f.f_code.co_filename
-    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
-
-
-def is_an_image(url):
-	if url[-3:] in ['jpg', 'png'] or url[-4:] in ['jpeg']:
-		return True
-	return False
-
-
-def get_avg_created_to_front():
-	cursor.execute('select AVG(discovered - created_utc)/60/60.0 from posts;')
-	return cursor.fetchone()[0]
-
-
-def get_avg_front_life():
-	cursor.execute('select AVG(lastseen - discovered)/60/60.0 from posts;')
-	return cursor.fetchone()[0]
-
-
-def is_id_in_db(sub):
-	cursor.execute('SELECT EXISTS(SELECT 1 FROM posts WHERE id=?)', (sub.id,))
-	return bool(cursor.fetchone()[0])
-	
-def add_sub_to_db(sub):
-	cursor.execute(
-		'INSERT INTO posts (id, title, url, subreddit, subreddit_id, created_utc, discovered) VALUES (?, ?, ?, ?, ?, ?, ?)',
-		(
-			sub.id,
-			sub.title,
-			sub.url,
-		   	str(sub.subreddit),
-		    sub.subreddit_id,
-		    int(sub.created_utc),
-			int(time.time())
-		)
-	)
-	conn.commit()
-
-def update_lastseen(sub):
-	cursor.execute(
-		'UPDATE posts SET lastseen=? WHERE id=?',
-		(int(time.time()), sub.id)
-	)
-	conn.commit()
-
 while True:
  
 	try:
@@ -100,6 +114,8 @@ while True:
 		total_sub_count = 0
 		new_sub_count = 0
 
+		print('')
+
 		for submission in submissions:
 			total_sub_count += 1
 
@@ -112,11 +128,11 @@ while True:
 			else:
 				update_lastseen(submission)
 
-		print('{}: Found {}/{} new posts, sleeping'.format(str(datetime.now()), new_sub_count, total_sub_count))
+		print('{}: Found {} of {} new posts, sleeping after report;'.format(str(datetime.now()), new_sub_count, total_sub_count))
+		print('Avg time to get to front page: {}'.format(human_time(get_avg_created_to_front())))
+		print('Avg life span on front page: {}'.format(human_time(get_avg_front_life())))
 		
 	except:
 		print_exception()
 	
-	print('Average time from created to /r/all top100: {} hrs'.format(get_avg_created_to_front()))
-	print('Average lifespan on front page: {} hrs'.format(get_avg_front_life()))
 	time.sleep(600)
